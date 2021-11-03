0e0d39b42716ee59b20eff883d3edc337ad21608
Diego Fernandes Affonso <diegofernandesaffonso@gmail.com>
2021-11-03 01:12:59 -0300

    Exercicio 2

---
+++ exercicio2.js
@@ -0,0 +1,6 @@
+// Crie um hashmap (ou um objeto simples em JavaScript) que contenha as funções elementares de adição, subtração, multiplicação e divisão.
+// Leve em consideração que a divisão não deve permitir dividendo 0
+// A função sum permite um array como parâmetro de entrada e adiciona todos os seus elementos.
+// A função de subtração permite um array como parâmetro de entrada e subtrai todos os seus elementos.
+// Função de multiplicação - Idem
+// A função de divisão aceita dois parâmetros: a e b.
